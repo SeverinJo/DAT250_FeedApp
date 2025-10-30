ee2afd05bccc0882d39ed47d0d466922d0a0e3d7
599061 <dag__aa@hotmail.com>
2025-10-30 22:50:49 +0100

    user service

--- backend/src/main/java/no/hvl/FeedApp/repositories/UserRepo.java
+++ backend/src/main/java/no/hvl/FeedApp/repositories/UserRepo.java
@@ -4,6 +4,11 @@ import no.hvl.FeedApp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
+    Optional<User> findById (Long id);
+    User findByUsername(String username);
+    void deleteById(Long id);
 }
--- backend/src/main/java/no/hvl/FeedApp/services/UserService.java
+++ backend/src/main/java/no/hvl/FeedApp/services/UserService.java
@@ -1,7 +1,37 @@
 package no.hvl.FeedApp.services;
 
+import lombok.RequiredArgsConstructor;
+import no.hvl.FeedApp.entities.User;
+import no.hvl.FeedApp.repositories.UserRepo;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepo userRepo;
+
+    public User registerUser (User user) {
+        return userRepo.save(user);
+    }
+
+    public Optional<User> findUserById(Long id) {
+        return userRepo.findById(id);
+    }
+
+    public User findUserByUsername (String username) {
+        return userRepo.findByUsername(username);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepo.findAll();
+    }
+
+    public void deleteUser(Long id) {
+        userRepo.deleteById(id);
+    }
+
 }
