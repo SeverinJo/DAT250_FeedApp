19dfb183344eb19fbeecaf1c8ffa53faa8f5254e
600901 <600901@stud.hvl.no>
2025-11-03 18:40:18 +0100

    added sample user for testing

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+-- example user data
+INSERT INTO feed_app.users (ID, USERNAME, EMAIL, HASHED_PASSWORD)
+VALUES (1, 'foo', 'foo@foo.com', '$2a$10$BK8LDWZfrLfNvW2Emx0oK.srLdkcYixmGrdm3td/Zv3FpFPn7InMm');
